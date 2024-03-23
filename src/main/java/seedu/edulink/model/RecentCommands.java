4c43ee71233ccad2152eec62aac2b3848c0fb0a2
dedsecrattle <itsprabxxx@gmail.com>
2024-03-24 01:36:18 +0800

    Fix Checkstyle

--- src/main/java/seedu/edulink/model/RecentCommands.java
+++ src/main/java/seedu/edulink/model/RecentCommands.java
@@ -5,39 +5,55 @@ import static java.util.Objects.requireNonNull;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
+/**
+ * Keeps track of the Recent Commands
+ * provided by Users
+ */
 public class RecentCommands {
-    private final String[] commands;
     public static final int LIMIT = 5;
+    private final String[] commands;
     private int counter;
 
-    public RecentCommands () {
+    /**
+     * Initializes RecentCommands with initially empty list of commands
+     * and a counter to keep track of no. of Commands Stored
+     */
+    public RecentCommands() {
         this.commands = new String[LIMIT];
         this.counter = 0;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if(obj == this) {
+        if (obj == this) {
             return true;
         } else if (!(obj instanceof RecentCommands)) {
             return false;
-        } else{
+        } else {
             RecentCommands other = (RecentCommands) obj;
             return Arrays.equals(this.commands, other.commands);
         }
     }
 
+
+    /**
+     * Adds a command to the List of Recent Commands
+     * @param command Command to be added in the List
+     */
     public void add(String command) {
         requireNonNull(command);
         if (counter == LIMIT) {
-            for(int i = 0; i < counter - 1; i++){
-                commands[i] = commands[i + 1];
-            }
-            commands[LIMIT - 1] = command;
+            removeOldestCommand();
+            commands[counter - 1] = command;
         } else {
             commands[counter] = command;
             counter++;
+        }
+    }
+
+    private void removeOldestCommand() {
+        for (int i = 0; i < counter - 1; i++) {
+            commands[i] = commands[i + 1];
         }
     }
 
@@ -52,8 +68,8 @@ public class RecentCommands {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < counter; i++){
-            if(i == counter - 1){
+        for (int i = 0; i < counter; i++) {
+            if (i == counter - 1) {
                 sb.append(commands[i]);
             } else {
                 sb.append(commands[i]).append(" ");
--- src/test/java/seedu/edulink/model/RecentCommandsTest.java
+++ src/test/java/seedu/edulink/model/RecentCommandsTest.java
@@ -1,10 +1,13 @@
 package seedu.edulink.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.edulink.testutil.Assert.assertThrows;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -19,16 +22,11 @@ public class RecentCommandsTest {
     }
 
     @Test
-    public void add_null_command_throwsException() {
-        assertThrows(NullPointerException.class, () -> recentCommands.add(null));
-    }
-
-    @Test
     public void commandsReturns_recent() {
-        ArrayList<String> acutal =  new ArrayList<>();
-        acutal.add("list");
-        acutal.add("delete 1");
-        assertEquals(acutal, recentCommands.getCommands());
+        ArrayList<String> actual = new ArrayList<>();
+        actual.add("list");
+        actual.add("delete 1");
+        assertEquals(actual, recentCommands.getCommands());
     }
 
     @Test
@@ -36,7 +34,7 @@ public class RecentCommandsTest {
         RecentCommands incorrect = new RecentCommands();
         incorrect.add("delete 2");
         incorrect.add("list");
-        RecentCommands correct= new RecentCommands();
+        RecentCommands correct = new RecentCommands();
         correct.add("delete 1");
         correct.add("list");
         assertNotEquals(recentCommands, incorrect);
@@ -55,5 +53,77 @@ public class RecentCommandsTest {
     public void toString_test() {
         assertNotEquals("tag i/A0265901E", recentCommands.toString());
         assertEquals("delete 1 list", recentCommands.toString());
+    }
+
+    @Test
+    public void constructor_noArgs_initializesEmptyArray() {
+        RecentCommands recentCommands = new RecentCommands();
+        assertEquals(0, recentCommands.getCommands().size());
+    }
+
+    @Test
+    public void equals_sameObject_returnsTrue() {
+        RecentCommands recentCommands = new RecentCommands();
+        assertTrue(recentCommands.equals(recentCommands));
+    }
+
+    @Test
+    public void equals_differentClass_returnsFalse() {
+        RecentCommands recentCommands = new RecentCommands();
+        assertFalse(recentCommands.equals(new Object()));
+    }
+
+    @Test
+    public void equals_sameCommands_returnsTrue() {
+        RecentCommands recentCommands1 = new RecentCommands();
+        RecentCommands recentCommands2 = new RecentCommands();
+        recentCommands1.add("command1");
+        recentCommands2.add("command1");
+        assertTrue(recentCommands1.equals(recentCommands2));
+    }
+
+    @Test
+    public void add_nullCommand_throwsNullPointerException() {
+        RecentCommands recentCommands = new RecentCommands();
+        assertThrows(NullPointerException.class, () -> recentCommands.add(null));
+    }
+
+    @Test
+    public void add_validCommands_addsCorrectly() {
+        RecentCommands recentCommands = new RecentCommands();
+        recentCommands.add("command1");
+        recentCommands.add("command2");
+        recentCommands.add("command3");
+        ArrayList<String> expectedCommands = new ArrayList<>(Arrays.asList("command3", "command2", "command1"));
+        assertEquals(expectedCommands, recentCommands.getCommands());
+    }
+
+    @Test
+    public void add_exceedsLimit_removesOldestCommand() {
+        RecentCommands recentCommands = new RecentCommands();
+        recentCommands.add("command1");
+        recentCommands.add("command2");
+        recentCommands.add("command3");
+        recentCommands.add("command4");
+        recentCommands.add("command5");
+        recentCommands.add("command6");
+        ArrayList<String> expectedCommands = new ArrayList<>(
+            Arrays.asList("command6", "command5", "command4", "command3", "command2"));
+        assertEquals(expectedCommands, recentCommands.getCommands());
+    }
+
+    @Test
+    public void toString_noCommands_returnsEmptyString() {
+        RecentCommands recentCommands = new RecentCommands();
+        assertEquals("", recentCommands.toString());
+    }
+
+    @Test
+    public void toString_withCommands_returnsCorrectString() {
+        RecentCommands recentCommands = new RecentCommands();
+        recentCommands.add("command1");
+        recentCommands.add("command2");
+        recentCommands.add("command3");
+        assertEquals("command1 command2 command3", recentCommands.toString());
     }
 }
