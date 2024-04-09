129f9a126d007e074f4f33a549f1742089dfbd40
dedsecrattle <itsprabxxx@gmail.com>
2024-04-10 02:01:51 +0800

    Fix Checkstyle

--- src/main/java/seedu/edulink/model/student/Email.java
+++ src/main/java/seedu/edulink/model/student/Email.java
@@ -9,6 +9,7 @@ import static seedu.edulink.commons.util.AppUtil.checkArgument;
  */
 public class Email {
 
+    public static final String VALIDATION_REGEX = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
     private static final String SPECIAL_CHARACTERS = "+_.-";
     public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain "
         + "and adhere to the following constraints:\n"
@@ -21,15 +22,6 @@ public class Email {
         + "    - end with a domain label at least 2 characters long\n"
         + "    - have each domain label start and end with alphanumeric characters\n"
         + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.";
-    // alphanumeric and special characters
-    private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]+"; // alphanumeric characters except underscore
-    private static final String LOCAL_PART_REGEX = "^" + ALPHANUMERIC_NO_UNDERSCORE + "([" + SPECIAL_CHARACTERS + "]"
-        + ALPHANUMERIC_NO_UNDERSCORE + ")*";
-    private static final String DOMAIN_PART_REGEX = ALPHANUMERIC_NO_UNDERSCORE
-        + "(-" + ALPHANUMERIC_NO_UNDERSCORE + ")*";
-    private static final String DOMAIN_LAST_PART_REGEX = "(" + DOMAIN_PART_REGEX + "){2,}$"; // At least two chars
-    private static final String DOMAIN_REGEX = "(" + DOMAIN_PART_REGEX + "\\.)*" + DOMAIN_LAST_PART_REGEX;
-    public static final String VALIDATION_REGEX = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
 
     public final String value;
 
