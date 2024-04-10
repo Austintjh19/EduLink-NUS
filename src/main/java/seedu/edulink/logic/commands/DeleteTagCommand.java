6b24b664bbc6f23e7004895a6c15aadf70dad41f
Li_Zizhen <e1075569@u.nus.edu>
2024-04-10 16:37:33 +0800

    Fix checkstyle

--- src/main/java/seedu/edulink/logic/commands/DeleteTagCommand.java
+++ src/main/java/seedu/edulink/logic/commands/DeleteTagCommand.java
@@ -25,7 +25,8 @@ public class DeleteTagCommand extends Command {
 
     public static final String MESSAGE_PERSON_NOTFOUND = "Can't find the Student you specified.";
     public static final String MESSAGE_DELETE_TAG_SUCCESS = "Deleted Tags: %1$s";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Delete tags(case sensitive) from the specific student. \n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Delete tags(case sensitive)"
+            + " from the specific student. \n"
             + "Parameters: " + PREFIX_ID + "ID " + PREFIX_TAG + "TAG \n"
             + "Example: " + COMMAND_WORD + " " + PREFIX_ID + "A0744231A "
             + PREFIX_TAG + "Tag1 " + PREFIX_TAG + "Tag2";
